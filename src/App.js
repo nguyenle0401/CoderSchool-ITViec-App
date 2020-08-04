0de70f37b4ae13f6f3a9f2cff1c09129a5e71800
Nguyen Le <nle20959@gmail.com>
2020-08-04 16:55:27 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Jobs from "./pages/Jobs";
 import Detail from "./pages/Detail";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
 
   return (
     <div className="App">
+      <Link to="/jobs">Click here to go to Jobs</Link>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/login" component={Login}/>
